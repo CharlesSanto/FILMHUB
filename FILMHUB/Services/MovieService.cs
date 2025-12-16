5c51c213a11391c74204573db43776220411ea7e
Charles Santos <coliveirasantos1234@gmail.com>
2025-12-16 18:59:35 -0300

    .

--- FILMHUB/Services/MovieService.cs
+++ FILMHUB/Services/MovieService.cs
@@ -14,28 +14,28 @@ public class MovieService : IMovieService
     
     public async Task<List<Movie>> GetPopularMoviesAsync()
     {
-        var popularMovies = await _client.GetFromJsonAsync<MovieApiResponse>("movie/popular");
+        var popularMovies = await _client.GetFromJsonAsync<MovieApiResponse>("movie/popular?language=pt-BR");
         
         return popularMovies.Results;
     }
     
     public async Task<List<Movie>> GetMoviesInTheaters()
     {
-        var theatersMovies = await _client.GetFromJsonAsync<MovieApiResponse>("movie/now_playing");
+        var theatersMovies = await _client.GetFromJsonAsync<MovieApiResponse>("movie/now_playing?language=pt-BR");
         
         return theatersMovies.Results;
     }
 
     public async Task<List<Movie>> GetTopRatedMovies()
     {
-        var topRated = await _client.GetFromJsonAsync<MovieApiResponse>("movie/top_rated");
+        var topRated = await _client.GetFromJsonAsync<MovieApiResponse>("movie/top_rated?language=pt-BR");
         
         return topRated.Results;
     }
 
     public async Task<List<Movie>> GetTrendingMovies()
     {
-        var trendingMovies = await _client.GetFromJsonAsync<MovieApiResponse>("trending/movie/week");
+        var trendingMovies = await _client.GetFromJsonAsync<MovieApiResponse>("trending/movie/week?language=pt-BR");
         
         return trendingMovies.Results;
     }
