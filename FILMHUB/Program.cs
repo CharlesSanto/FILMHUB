54171d37a309cf88f93512c872c3ea20e7108a8d
Charles Santos <coliveirasantos1234@gmail.com>
2026-01-06 15:50:52 -0300

    .

--- FILMHUB/Program.cs
+++ FILMHUB/Program.cs
@@ -62,5 +62,11 @@ app.MapControllerRoute(
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 
 app.Run();
